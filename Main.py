5f2d04b677c6070098d5d86fc63cccb8c9627b51
Eddyshmis <jazzir03@gmail.com>
2023-11-05 17:44:56 -0700

    first commit

---
+++ Main.py
@@ -0,0 +1,35 @@
+import turtle as tl
+import classes
+import time
+import socket
+window = tl.Screen()
+player = tl.Turtle()
+
+
+size_multiplier = 6
+width_window,height_window = window.screensize()
+
+window.tracer(0,0)
+
+
+play_solo_btn = classes.Button(size_multiplier,"square")
+play_multiplayer_btn = classes.Button(size_multiplier,"square")
+
+play_multiplayer_btn.place((play_multiplayer_btn.turtle.xcor() + (30*size_multiplier)),play_multiplayer_btn.turtle.ycor())
+play_solo_btn.place((play_solo_btn.turtle.xcor() - (30*size_multiplier)),play_multiplayer_btn.turtle.ycor())
+
+play_multiplayer_btn.lable("Multiplayer",'red',30)
+play_solo_btn.lable("solo",'red',30)
+
+def solo_play(x,y):
+    window.clear()
+def Multiplayer_play(x,y):
+    window.clear()
+
+play_solo_btn.click_on(solo_play)
+play_multiplayer_btn.click_on(Multiplayer_play)
+
+
+
+window.update()
+window.mainloop()
---
+++ classes.py
@@ -0,0 +1,38 @@
+import turtle as tl
+import time
+import socket
+class Button():
+    def __init__(self,size_multiplier,shape,color = "black"):
+        self.size_multiplier = size_multiplier
+        self.shape = shape
+        self.turtle = tl.Turtle()
+
+
+        self.turtle.speed(0)
+        self.turtle.color(color)
+        self.turtle.shapesize((1 * size_multiplier),(1 * size_multiplier))
+        self.turtle.up()
+        self.turtle.shape(f"{shape}")
+        self.turtle.hideturtle()
+    def place(self,x:float = 0,y:float = 0):
+        self.x = x
+        self.y = y
+        self.turtle.goto(x,y)
+        self.turtle.showturtle()
+    def click_on(self,func):
+        '''add x,y parameters for the inputed function
+        \ndef print_test(x,y):
+            print("works")'''
+        self.turtle.onclick(func)
+    def click_off(self):
+        self.turtle.onclick(None)
+    def lable(self,text,color,size = 30):
+        self.turtle.goto(self.turtle.xcor(),(self.turtle.ycor() + (10*self.size_multiplier)))
+        self.turtle.pencolor(color)
+        self.turtle.write(f"{text}",False,"center",("Arial",size,"normal"))
+        self.turtle.goto(self.turtle.xcor(),(self.turtle.ycor() - (10*self.size_multiplier)))
+# test_turt = Button(2.5,"square")
+# test_turt.place(100,10)
+
+
+# tl.mainloop()
